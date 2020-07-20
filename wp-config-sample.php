7446715074f7a3fd385f6276fbfc11cd800841f9
Peter <p-w@users.noreply.github.com>
2020-07-20 14:50:48 +0200

    Create wp-config-sample.php
    sample configuration

---
+++ wp-config-sample.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Core configuration for WordPress Versandgigant WP Simple Secure SMTP Mails
+ *
+ * The settings include: 
+ *
+ * * SMTP Configuration,
+ * * SMTp Authentification.
+ */
+
+// ** SMTP Configuration ** //
+/**   You're usually getting them from your webhoster. **/
+
+/**
+ * USE SMTP instead of php mail() function
+ */
+define( 'USE_SMTP', 'datenbankname_hier_einfuegen' );
+
+
+/**
+ * set security schema, tls or ssl
+ */
+define( 'SMTP_SECURE', 'datenbankname_hier_einfuegen' );
+
+/**
+ * enable SMTP authentication
+ */
+define( 'SMTP_AUTH', 'datenbankname_hier_einfuegen' );
+
+/**
+ * set the SMTP server port, 25 or 587
+ */
+define( 'SMTP_PORT', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP server host
+ */
+define( 'SMTP_HOST', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP server username
+ */
+define( 'SMTP_USER', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP server password
+ */
+define( 'SMTP_PASS', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP From email address
+ */
+define( 'SMTP_FROM', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP From name
+ */
+define( 'SMTP_NAME', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP Reply-To email address
+ */
+define( 'SMTP_REPLYTO', 'datenbankname_hier_einfuegen' );
+
+/**
+ * SMTP Reply-To name
+ */
+define( 'SMTP_REPLYTO_NAME', 'datenbankname_hier_einfuegen' );
+
+/**
+ * set debug level, 1, 2 or 0
+ */
+define( 'SMTP_DEBUG', 'datenbankname_hier_einfuegen' );
